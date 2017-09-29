96986e0b7b756d739faef433233864faab03c090
Justin Calamari <justin.calamari@gmail.com>
2017-09-29 02:24:48 -0400

    add sampledatabase.py

---
+++ sampledatabase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from databasesearcher import DatabaseSearcher
+from datapublisher import DataPublisher
+from pymongo import MongoClient
+
+class SampleDatabase(object):
+
+    def __init__(self, hostname=None, db='sampleDB', collection='samples'):
+        c = MongoClient(hostname)
+        collection = c[db][collection]
+        self.searcher = DatabaseSearcher(collection)
+        self.publisher = DataPublisher(collection)
+
+    def search(self, **kwargs):
+        return self.searcher.search(**kwargs)
+
+    def publish(self, filename):
+        self.publisher.publish(filename)
