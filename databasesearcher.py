3d6c4dcb28bed6b1c2c509be0fcdf26bdcad96f6
Justin Calamari <justin.calamari@gmail.com>
2017-09-29 02:24:03 -0400

    Add databasesearcher.py and searchresult.py

---
+++ databasesearcher.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+from datetime import datetime
+from searchresult import SearchResult
+
+
+class DatabaseSearcher(object):
+    """
+    Seach a database.
+    """
+
+    def __init__(self, collection):
+        """
+        Create a DatabaseSearcher.
+        """
+        self.collection = collection
+
+
+    @classmethod
+    def parse_date(cls, date):
+        """
+        Convert a string in 'YYYY-MM-DD' format to a datetime object.
+        """
+        date = date.split('-')
+        date = [int(i) for i in date]
+        return datetime(date[0], date[1], date[2])
+    
+    
+    @classmethod
+    def date_range(cls, startdate=None, enddate=None):
+        range_ = {}
+        if startdate:
+            start = cls.parse_date(startdate)
+            range_['$gte'] = start
+        if enddate:
+            end = cls.parse_date(enddate)
+            range_['$lte'] = end
+    
+        if range_:
+            return {'date': range_}
+        else:
+            return {}
+    
+    
+    def search(self, **kwargs):
+        """
+        Search the database for entries with the specified key, value pairs.
+        Returns a cursor with the results.
+        """
+        query = kwargs
+        dr = self.date_range(query.pop('startdate', None),
+                        query.pop('enddate', None))
+        query.update(dr)
+        return SearchResult(list(self.collection.find(query, {'_id': 0})))
---
+++ searchresult.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import pandas as pd
+from pprint import pformat
+from functools import reduce
+
+class SearchResult(object):
+    """
+    An object containing the matching results of a search on the database.
+    """
+
+    def __init__(self, results):
+        """
+        Create a SearchResult.
+        """
+        self.results = pd.DataFrame(results)
+        if self.results.size == 0:
+            return
+        self.results.sort_values(list(self.results.columns), inplace=True)
+        self.results.reset_index(drop=True, inplace=True)
+
+
+    def __repr__(self):
+        return pformat(self.results.T.to_dict())
+
+
+    def __str__(self):
+        return pformat(self.results.T.to_dict())
+            
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.results.equals(other.results)
+        return False
+
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+    def filter(self, indices):
+        """
+        Filter the search results.
+        Returns new SearchResult with only the filtered results.
+        """
+        df = self.results.filter(items=indices, axis=0)
+        return SearchResult(df.reset_index(drop=True))
+
+
+    def download(self, filename):
+        """
+        Download the search results as a spreadsheet.
+        """
+        frames = []
+        for name in self.results:
+            frames.append(self.results[name].rename(name.replace('_', ' ').title()).to_frame())
+        df = reduce(lambda x, y: x.join(y), frames)
+
+        writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+        df.to_excel(writer, index=False)
+        sheet = writer.sheets['Sheet1']
+        for i, name in enumerate(df):
+            width = max(len(str(val)) for val in df[name])
+            width = max(width, len(name)) + 1
+            sheet.set_column(i, i, width)
+        writer.save()
