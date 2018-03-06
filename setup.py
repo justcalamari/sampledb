3bac5489621efccf54edbed992f780431ea146a0
Justin Calamari <justin.calamari@gmail.com>
2018-03-05 19:49:56 -0500

    FIX: properly install xsh files in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ setup(
     name='sampledb',
     version='0.1.1',
     packages=find_packages(),
+    package_data={'sampledb': ['*.xsh']},
     entry_points={
-        'console_scripts':
-        ['publish_samples=sampledb.reader:publish_samples',
-         'download_samples=sampledb.reader:download_samples'],
+        'console_scripts': [
+            'publish_samples=sampledb.reader:publish_samples',
+            'download_samples=sampledb.reader:download_samples',
+            'clean=sampledb.generate_qr:clean',
+        ],
     },
     description='database search and publish',
     zip_safe=False,
